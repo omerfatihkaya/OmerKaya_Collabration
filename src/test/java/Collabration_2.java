6dea341d49f3351ba5985f9476ca39485b5ac52b
OmerFatihKAYA <av.omerfatihkaya@gmail.com>
2023-10-22 15:40:56 +0300

    ekleme

--- src/test/java/Collabration_2.java
+++ src/test/java/Collabration_2.java
@@ -1,5 +1,14 @@
 public class Collabration_2 {
     public static void main(String[] args) {
         System.out.println("merhaba Github2");
+
+
+        int a = 300;
+        int b = 400;
+        System.out.println(a+b);
+
     }
+
+
+
 }
